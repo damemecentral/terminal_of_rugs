06a5093e3e8f9fcb103a3ad7a46854171213b9ba
damemecentral <shipesol@proton.me>
2024-10-23 02:31:51 +0200

    Update api_commands.ts

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -4,14 +4,5 @@ import { getProjects } from '../api';
 import { getQuote } from '../api';
 import { getReadme } from '../api';
 import { getWeather } from '../api';
-
-export const projects = async (args: string[]): Promise<string> => {
-  const projects = await getProjects();
-  return projects
-    .map(
-      (repo) =>
-        `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
-    )
-    .join('\n');
 };
 
