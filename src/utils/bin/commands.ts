fd42c12e5dcfd6062790a484c0285d292565899a
damemecentral <shipesol@proton.me>
2024-10-24 10:47:16 +0200

    Update commands.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -52,13 +52,13 @@ export const twitter = async (args?: string[]): Promise<string> => {
 
 // Dex
 export const dexscreener = async (args?: string[]): Promise<string> => {
-  window.open('https://dexscreener.com/solana/EqUaAuEdakVBKGbiF95as7Amk2sRV1SfxFBGX4THpump', '_blank'); 
+  window.open('https://dexscreener.com/solana/G7GBBDKGv8fTXiNvhvCBp2mY9Dqym8eMLHKT3fYPpump', '_blank'); 
   return `Dexscreener Loading...`;
 };
 
 // Contract 
 export const ca = async (args: string[]): Promise<string> => {
-  return `EqUaAuEdakVBKGbiF95as7Amk2sRV1SfxFBGX4THpump`;
+  return `G7GBBDKGv8fTXiNvhvCBp2mY9Dqym8eMLHKT3fYPpump`;
 };
 
 // Fake News
@@ -182,6 +182,6 @@ export const banner = (args?: string[]): string => {
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display your campaign summary.
 
-ca: EqUaAuEdakVBKGbiF95as7Amk2sRV1SfxFBGX4THpump
+ca: G7GBBDKGv8fTXiNvhvCBp2mY9Dqym8eMLHKT3fYPpump
 `;
 };
